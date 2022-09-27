39b57fd74b7b8d61856d1d162723bbc66d969e70
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-09-27 11:19:02 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ function getNumberInfo() {
     if (isNaN(num) || num.length == 0 || num<2 || num>9 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
         txt += `Invalid Input.  Please enter a whole number between 2 and 9 without leading zeros.`;
     } else {
-        txt += `You have entered the number ${num}.<p>`;
-        txt += `Sum of all 0 to ${num} pandigital numbers is ${substringDivisibility(num)}.`;
+       txt += `You have entered the number ${num}.<p>`;
     }
 
     // Display Information in the Browser
